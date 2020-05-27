3ecec8d505d15d1fcd65061e1373d04f61e32582
VIJAY <VIJAY@DESKTOP-GJ02TEM>
2020-05-27 20:27:27 +0530

    file changes

---
+++ src/main/java/com/sreenu/online/ecommerce/model/ProductModel.java
@@ -0,0 +1,41 @@
+package com.sreenu.online.ecommerce.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+import org.springframework.stereotype.Component;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ProductModel {
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    @ApiModelProperty(notes = "The database generated ProductModel ID")
+	    private Integer modelId;
+	    @Version
+	    @ApiModelProperty(notes = "The auto-generated version of the ProductModel")
+	    private Integer version;
+	    @ApiModelProperty(notes = "The application-specific ProductModel ID")
+	    private String productModelId;
+	
+
+}
---
+++ src/main/java/com/sreenu/online/ecommerce/service/ProductService.java
@@ -0,0 +1,30 @@
+package com.sreenu.online.ecommerce.service;
+
+import org.springframework.stereotype.Service;
+
+import com.sreenu.online.ecommerce.model.Product;
+
+@Service
+public class ProductService {
+
+	public Iterable listAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Product getProductById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void saveProduct(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteProduct(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
